0adfc942c2c36e4e635ed149ec4fc37de53fd49f
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-06 11:01:02 -0500

    Update Matchup.js

--- Main/client/src/pages/Matchup.js
+++ Main/client/src/pages/Matchup.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useMutation, useQuery } from '@apollo/client';
-import { QUERY_TECH } from '../utils/queries';
+import { QUERY_PLACES } from '../utils/queries';
 import { CREATE_MATCHUP } from '../utils/mutations';
 
 const Matchup = () => {
-  const { loading, data } = useQuery(QUERY_TECH);
+  const { loading, data } = useQuery(QUERY_PLACES);
 
-  const techList = data?.tech || [];
+  const placesList = data?.places || [];
 
   const [formData, setFormData] = useState({
-    tech1: 'JavaScript',
-    tech2: 'JavaScript',
+    places1: 'JavaScript',
+    places2: 'JavaScript',
   });
   let history = useHistory();
 
@@ -44,38 +44,14 @@ const Matchup = () => {
   return (
     <div className="card bg-white card-rounded w-25">
       <div className="card-header bg-dark text-center">
-        <h1>Let's create a matchup!</h1>
+        <h1>Dog-Friendly Places Near You</h1>
       </div>
       <div className="card-body m-5">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <label>Tech 1: </label>
-            <select name="tech1" onChange={handleInputChange}>
-              {techList.map((tech) => {
-                return (
-                  <option key={tech._id} value={tech.name}>
-                    {tech.name}
-                  </option>
-                );
-              })}
-            </select>
-            <label>Tech 2: </label>
-            <select name="tech2" onChange={handleInputChange}>
-              {techList.map((tech) => {
-                return (
-                  <option key={tech._id} value={tech.name}>
-                    {tech.name}
-                  </option>
-                );
-              })}
-            </select>
-            <button className="btn btn-danger" type="submit">
-              Create Matchup!
-            </button>
-          </form>
-        )}
+
+    
+    //this section needs to be properly coded
+    
+    
       </div>
       {error && <div>Something went wrong...</div>}
     </div>
