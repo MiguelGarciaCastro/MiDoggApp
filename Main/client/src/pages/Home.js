dee7cd1dac3ff8964617f6b4d2d1d5b3c61d6b41
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-06 10:58:40 -0500

    Update Home.js

--- Main/client/src/pages/Home.js
+++ Main/client/src/pages/Home.js
@@ -12,30 +12,20 @@ const Home = () => {
   return (
     <div className="card bg-white card-rounded w-50">
       <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
+        <h1>Welcome to MiDogApp</h1>
       </div>
       <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
+        <h2>Select a City Near You to Find Dog-Friendly Activities</h2> 
+    
+    //this section needs to be properly coded
+    
+    
+    
       </div>
       <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
+        <h2>Want to add a dog-friendly place or activity to our database?</h2>
         <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
+          <button className="btn btn-lg btn-danger">Add to our database!</button>
         </Link>
       </div>
     </div>
