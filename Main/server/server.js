d9fb3f74d0b3ebb057789663442911ad08f51448
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-06 10:52:11 -0500

    Update server.js

--- Main/server/server.js
+++ Main/server/server.js
@@ -16,7 +16,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// if in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
