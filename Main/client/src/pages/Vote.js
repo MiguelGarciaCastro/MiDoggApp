fd5e0bbc8428532310e1bfe33498141f062e87c7
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-06 11:07:06 -0500

    Update Vote.js

--- Main/client/src/pages/Vote.js
+++ Main/client/src/pages/Vote.js
@@ -12,47 +12,21 @@ const Vote = () => {
 
   const matchup = data?.matchups || [];
 
-  const [createVote, { error }] = useMutation(CREATE_VOTE);
-
-  const handleVote = async (techNum) => {
-    try {
-      await createVote({
-        variables: { _id: id, techNum: techNum },
-      });
-    } catch (err) {
-      console.error(err);
-    }
   };
 
   return (
     <div className="card bg-white card-rounded w-50">
       <div className="card-header bg-dark text-center">
-        <h1>Here is the matchup!</h1>
+        <h1>Here is the Full List of Dog-Friendly MI Places</h1>
       </div>
       {loading ? (
         <div>Loading...</div>
-      ) : (
+    
         <div className="card-body text-center mt-3">
-          <h2>
-            {matchup[0].tech1} vs. {matchup[0].tech2}
-          </h2>
-          <h3>
-            {matchup[0].tech1_votes} : {matchup[0].tech2_votes}
-          </h3>
-          <button className="btn btn-info" onClick={() => handleVote(1)}>
-            Vote for {matchup[0].tech1}
-          </button>{' '}
-          <button className="btn btn-info" onClick={() => handleVote(2)}>
-            Vote for {matchup[0].tech2}
-          </button>
-          <div className="card-footer text-center m-3">
-            <br></br>
-            <Link to="/">
-              <button className="btn btn-lg btn-danger">
-                View all matchups
-              </button>
-            </Link>
-          </div>
+
+    //needs code
+    
+    
         </div>
       )}
       {error && <div>Something went wrong...</div>}
