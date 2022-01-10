83c91c675a1ac5e4f8b238872b5a02c7d17a4f00
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-09 20:18:38 -0500

    Update index.js

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import cheers from '../../public/images/cheers.png';
+import cheers from '../../public/images/logo.png';
 import Auth from '../../utils/auth';
 import { Link } from "react-router-dom";
 
@@ -41,8 +41,8 @@ export default function Nav() {
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <Link className="navbar-item" to="/">BrewMap
-                    <img alt="cheers" src={cheers} width="28" height="28"></img>
+                <Link className="navbar-item" to="/">MiDogApp
+                    <img alt="logo" src={logo} width="28" height="28"></img>
                 </Link>
 
                 <Link role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
