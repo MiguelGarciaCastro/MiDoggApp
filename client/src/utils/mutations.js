ac707f74e964da3758db0df5d831d26f70725b24
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-11 00:52:08 -0500

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,21 +24,6 @@ export const ADD_USER = gql`
   }
 `;
 
-// for future Friending implementation
-export const ADD_FRIEND = gql`
-  mutation addFriend($id: ID!) {
-    addFriend(friendId: $id) {
-      _id
-      username
-      friendCount
-      friends {
-        _id
-        username
-      }
-    }
-  }
-`;
-
 export const ADD_POST = gql`
   mutation addPost($body: String!) {
     addPost(body: $body) {
@@ -65,19 +50,108 @@ export const ADD_COMMENT = gql`
   }
 `;
 
-export const ADD_BREWERY = gql`
-  mutation addBrewery($id: ID!) {
-    addBrewery(id: $id) {
+export const ADD_CITY = gql`
+  mutation addCity($id: String) {
+    addCity(id: String) {
         id
     }
   }
 `;
 
-
-export const REMOVE_BREWERY = gql`
-  mutation removeBrewery($id: ID!) {
-    removeBrewery(id: $id) {
+export const REMOVE_POST = gql`
+  mutation removePost($id: ID!) {
+    removePost(id: $id) {
       id
+    }
+  }
+`;
+
+export const ADD_GROOMER = gql`
+  mutation addGroomer($id: String) {
+    addGrooomer(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_STORE = gql`
+  mutation addStore($id: String) {
+    addStore(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_BEACH = gql`
+  mutation addBeach($id: String) {
+    addBeach(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_VETERINARIAN = gql`
+  mutation addVeterinarian($id: String) {
+    addVeterinarian(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_CAMPGROUND = gql`
+  mutation addCampground($id: String) {
+    addCampground(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_RESTAURANT = gql`
+  mutation addRestaurant($id: String) {
+    addRestaurant(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_HOTEL = gql`
+  mutation addHotel($id: String) {
+    addHotel(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_BOARDING = gql`
+  mutation addBoarding($id: String) {
+    addBoarding(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_TRAIL = gql`
+  mutation addTrail($id: String) {
+    addTrail(id: String) {
+        id
+        address
+    }
+  }
+`;
+
+export const ADD_PARK = gql`
+  mutation addPark($id: String) {
+    addPark(id: String) {
+        id
+        address
     }
   }
 `;
