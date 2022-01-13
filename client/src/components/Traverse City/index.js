84dbb140e460c1da838ac5fe7f1d797088d38816
TS93 <tschram93@yahoo.com>
2022-01-13 01:19:59 -0500

    Traversr City completed

--- client/src/components/Traverse City/index.js
+++ client/src/components/Traverse City/index.js
@@ -1,29 +1,45 @@
- 
 import React from 'react';
 
 export default function Nav() {
-    return (
-        <section class="hero is-fullheight">
-                <div class="hero-body">
-                    <div class="container has-text-centered">
-                        <div class="columns is-8 is-variable ">
-                            <div class="column is-two-thirds has-text-left">
-                                <h1 class="title is-1">Traverse City</h1>
-                                <p class="is-size-4">Dog-Friendly Places and Activities</p>
-                                <p class="is-size-5">Restaurants: </p>
-                                <p class="is-size-5">Veterinarians: </p>
-                                <p class="is-size-5">Beaches: Zoll Street Beach</p>
-                                <p class="is-size-5">Campgrounds: Timber Ridge RV and Recreation Resort, Ranch Rudolf</p>
-                                <p class="is-size-5">Boarding Facilities: SunDog Boarding Kennel, Aunt Tara's Creative Canine Solutions</p>
-                                <p class="is-size-5">Stores: Grand Traverse Pet Supply, PetSmart</p>
-                                <p class="is-size-5">Groomers: Doggie Stylez,  Backcountry Dog Salon</p>
-                                <p class="is-size-5">Hotels: Sleep Inn and Suites Acme, Travelodge, Traverse Bay Inn </p>
-                                <p class="is-size-5">Parks: Keith J. Charters Traverse City State Park</p>
-                                <p class="is-size-5">Trails: Keith J. Charters Traverse City State Park</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-    )
+	return (
+		<section class='hero is-fullheight'>
+			<div class='hero-body'>
+				<div class='container has-text-centered'>
+					<div class='columns is-8 is-variable '>
+						<div class='column is-two-thirds has-text-left'>
+							<h1 class='title is-1'>Traverse City</h1>
+							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
+							<p class='is-size-5'>Restaurants: The Filling Station Microbrewery, The Little Fleet, Moomers Homemade Ice Cream, Red Mesa Grill, Fire Fly, Taproot Cider House, Jolly Pumpkin Restaurant and Brewery, Chateau Chantal Winery</p>
+							<p class='is-size-5'>
+								Veterinarians: Merchant Logo BayWest Animal Clinic, Oakwood Veterinary Hospital
+							</p>
+							<p class='is-size-5'>Beaches: Zoll Street Beach</p>
+							<p class='is-size-5'>
+								Campgrounds: Timber Ridge RV and Recreation Resort, Ranch Rudolf
+							</p>
+							<p class='is-size-5'>
+								Boarding Facilities: SunDog Boarding Kennel, Aunt Tara's
+								Creative Canine Solutions
+							</p>
+							<p class='is-size-5'>
+								Stores: Grand Traverse Pet Supply, PetSmart
+							</p>
+							<p class='is-size-5'>
+								Groomers: Doggie Stylez, Backcountry Dog Salon
+							</p>
+							<p class='is-size-5'>
+								Hotels: Sleep Inn and Suites Acme, Travelodge, Traverse Bay Inn{' '}
+							</p>
+							<p class='is-size-5'>
+								Parks: Keith J. Charters Traverse City State Park
+							</p>
+							<p class='is-size-5'>
+								Trails: Keith J. Charters Traverse City State Park
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
