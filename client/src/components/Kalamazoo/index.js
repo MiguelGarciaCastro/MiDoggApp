96e0d9a05a84b685c062674ae6e32f56e29ef2af
TS93 <tschram93@yahoo.com>
2022-01-13 16:58:31 -0500

    parks and trails left

--- client/src/components/Kalamazoo/index.js
+++ client/src/components/Kalamazoo/index.js
@@ -1,29 +1,45 @@
-
 import React from 'react';
 
 export default function Nav() {
-    return (
-        <section class="hero is-fullheight">
-                <div class="hero-body">
-                    <div class="container has-text-centered">
-                        <div class="columns is-8 is-variable ">
-                            <div class="column is-two-thirds has-text-left">
-                                <h1 class="title is-1">Kalamazoo</h1>
-                                <p class="is-size-4">Dog-Friendly Places and Activities</p>
-                                <p class="is-size-5">Restaurants: </p>
-                                <p class="is-size-5">Veterinarians: </p>
-                                <p class="is-size-5">Beaches: </p>
-                                <p class="is-size-5">Campgrounds: </p>
-                                <p class="is-size-5">Boarding Facilities: </p>
-                                <p class="is-size-5">Stores: </p>
-                                <p class="is-size-5">Groomers: </p>
-                                <p class="is-size-5">Hotels: </p>
-                                <p class="is-size-5">Parks: </p>
-                                <p class="is-size-5">Trails: </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-    )
-} 
+	return (
+		<section class='hero is-fullheight'>
+			<div class='hero-body'>
+				<div class='container has-text-centered'>
+					<div class='columns is-8 is-variable '>
+						<div class='column is-two-thirds has-text-left'>
+							<h1 class='title is-1'>Kalamazoo</h1>
+							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
+							<p class='is-size-5'>
+								Restaurants: Boatyard Brewing Company, Green Top Tavern, HopCat,
+								The Thirsty Hound, O'Duffy's Pub, Principle Food and Drink,
+								Walnut and Park Cafe, Water Street Coffee Joint Downtown
+							</p>
+							<p class='is-size-5'>
+								Veterinarians: Eighth Street Veterinary Care, Kalamazoo Animal
+								Hospital PC, Pet Vet Family Pet Care Center,{' '}
+							</p>
+							<p class='is-size-5'>
+								Boarding Facilities: Camp Fido Dog Daycare, Camp Ravenwood
+								Kennels, Happytail Boarding Kennels
+							</p>
+							<p class='is-size-5'>
+								Stores: Bark's 5th Avenue, Chow Hound Pet Supplies, The Dog
+								Dive,PetSmart, Zeus + Blu{' '}
+							</p>
+							<p class='is-size-5'>
+								Groomers: Bark's 5th Avenue, Bone Appetit Bakery, Jan's Custom
+								Grooming, Pawriffic Grooming Salon, Plush Puppy, Wags by Connie
+							</p>
+							<p class='is-size-5'>
+								Hotels: Baymont Inn and Suites, Candlewood Suites, Econo Lodge,
+								Quality Inn, Staybridge Suites
+							</p>
+							<p class='is-size-5'>Parks: </p>
+							<p class='is-size-5'>Trails: </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
