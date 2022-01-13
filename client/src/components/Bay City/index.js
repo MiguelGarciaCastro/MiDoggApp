f37b3e67c720e1fcec4b17325f4236f0a67f7342
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-12 21:11:39 -0500

    Update Bay City Component

--- client/src/components/Bay City/index.js
+++ client/src/components/Bay City/index.js
@@ -1,1 +1,28 @@
-  
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <section class="hero is-fullheight">
+                <div class="hero-body">
+                    <div class="container has-text-centered">
+                        <div class="columns is-8 is-variable ">
+                            <div class="column is-two-thirds has-text-left">
+                                <h1 class="title is-1">Bay City</h1>
+                                <p class="is-size-4">Dog-Friendly Places and Activities</p>
+                                <p class="is-size-5">Restaurants: AW Drive-In, Souper Cafe</p>
+                                <p class="is-size-5">Veterinarians: At Bay Animal Hospital</p>
+                                <p class="is-size-5">Beaches: Bay City State Park</p>
+                                <p class="is-size-5">Campgrounds: Bay City State Park</p>
+                                <p class="is-size-5">Boarding Facilities: KBs K9 Training and Spa</p>
+                                <p class="is-size-5">Stores: PetSmart</p>
+                                <p class="is-size-5">Groomers: Doggy Groom Shop, Rebecca Snyders Grooming</p>
+                                <p class="is-size-5">Hotels: Quality Inn and Suites</p>
+                                <p class="is-size-5">Parks: Bay City State Park</p>
+                                <p class="is-size-5">Trails: Bay City State Park</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    )
+}
