5a1d0c3c1ed6c04f2910b73d75d43dfc1ad7a7f5
TS93 <tschram93@yahoo.com>
2022-01-13 16:34:27 -0500

    kennels, stores, groomers, hotels

--- client/src/components/Marquette/index.js
+++ client/src/components/Marquette/index.js
@@ -9,18 +9,29 @@ export default function Nav() {
 						<div class='column is-two-thirds has-text-left'>
 							<h1 class='title is-1'>Marquette</h1>
 							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
-							<p class='is-size-5'>Restaurants: The Delft Bistro, Dia De Los Tacos, Donckers, Kildare Irish American, Portside Inn, Lake Superior Smokehouse Brewpub </p>
-							<p class='is-size-5'>Veterinarians: Animal Medical Center, Marquette Veterinary Clinic, Bayshore Veterinary Hospital</p>
-							<p class='is-size-5'>Beaches: </p>
-							<p class='is-size-5'>Campgrounds: </p>
 							<p class='is-size-5'>
-								Boarding Facilities:
+								Restaurants: The Delft Bistro, Dia De Los Tacos, Donckers,
+								Kildare Irish American, Portside Inn, Lake Superior Smokehouse
+								Brewpub{' '}
 							</p>
-							<p class='is-size-5'>Stores: </p>
 							<p class='is-size-5'>
-								Groomers: 
+								Veterinarians: Animal Medical Center, Marquette Veterinary
+								Clinic, Bayshore Veterinary Hospital
 							</p>
-							<p class='is-size-5'>Hotels: </p>
+							<p class='is-size-5'>Beaches: Marquette South Beach</p>
+							<p class='is-size-5'>
+								Campgrounds: Gitche Gumee RV Park and Campground, Tourist Park
+								Campground
+							</p>
+							<p class='is-size-5'>
+								Boarding Facilities: The Animal Inn, Pet Sitters Plus Kennels,
+								Poets Pet Parlor,
+							</p>
+							<p class='is-size-5'>
+								Stores: Claws N Paws, Happy Dog Grooming Salon , PetSmart
+							</p>
+							<p class='is-size-5'>Groomers: Poets Pet Parlor</p>
+							<p class='is-size-5'>Hotels: Econo Lodge Lakeside, Magnuson Hotel Country Inn, My Place Hotel Marquette, Quality Inn, Staybridge Suites</p>
 							<p class='is-size-5'>Parks: </p>
 							<p class='is-size-5'>Trails: </p>
 						</div>
