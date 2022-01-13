b527b1c1764e5338832f77d76e7fb9010317155c
TS93 <tschram93@yahoo.com>
2022-01-13 17:12:01 -0500

    vets

--- client/src/components/Detroit/index.js
+++ client/src/components/Detroit/index.js
@@ -1,29 +1,37 @@
-
 import React from 'react';
 
 export default function Nav() {
-    return (
-        <section class="hero is-fullheight">
-                <div class="hero-body">
-                    <div class="container has-text-centered">
-                        <div class="columns is-8 is-variable ">
-                            <div class="column is-two-thirds has-text-left">
-                                <h1 class="title is-1">Detroit</h1>
-                                <p class="is-size-4">Dog-Friendly Places and Activities</p>
-                                <p class="is-size-5">Restaurants: ASHE Supply Co, The Brooklyn Street Local, Detroit Shipping Company, Germack Coffee and Tea Shop, Jolly Pumpkin, Mercury Burger and Bar, Motor City Brewing Works, Nancy Whiskey Pub, Niki's, Woodbridge Pub </p>
-                                <p class="is-size-5">Veterinarians: </p>
-                                <p class="is-size-5">Beaches: </p>
-                                <p class="is-size-5">Campgrounds: </p>
-                                <p class="is-size-5">Boarding Facilities: </p>
-                                <p class="is-size-5">Stores: </p>
-                                <p class="is-size-5">Groomers: </p>
-                                <p class="is-size-5">Hotels: </p>
-                                <p class="is-size-5">Parks: </p>
-                                <p class="is-size-5">Trails: </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-    )
+	return (
+		<section class='hero is-fullheight'>
+			<div class='hero-body'>
+				<div class='container has-text-centered'>
+					<div class='columns is-8 is-variable '>
+						<div class='column is-two-thirds has-text-left'>
+							<h1 class='title is-1'>Detroit</h1>
+							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
+							<p class='is-size-5'>
+								Restaurants: ASHE Supply Co, The Brooklyn Street Local, Detroit
+								Shipping Company, Germack Coffee and Tea Shop, Jolly Pumpkin,
+								Mercury Burger and Bar, Motor City Brewing Works, Nancy Whiskey
+								Pub, Niki's, Woodbridge Pub{' '}
+							</p>
+							<p class='is-size-5'>
+								Veterinarians: Francis Animal Hospital and Grooming Salon,
+								Grosse Pointe Animal Clinic, Jefferson Veterinary Center,
+								Michigan Animal Hospital,
+							</p>
+							<p class='is-size-5'>Beaches: </p>
+							<p class='is-size-5'>Campgrounds: </p>
+							<p class='is-size-5'>Boarding Facilities: </p>
+							<p class='is-size-5'>Stores: </p>
+							<p class='is-size-5'>Groomers: </p>
+							<p class='is-size-5'>Hotels: </p>
+							<p class='is-size-5'>Parks: </p>
+							<p class='is-size-5'>Trails: </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
