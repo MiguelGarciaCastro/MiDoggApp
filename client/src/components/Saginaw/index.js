13101e345478ae59772f1a34f9caf4c5f742baa6
TS93 <tschram93@yahoo.com>
2022-01-13 00:48:39 -0500

    Saginaw Complete

--- client/src/components/Saginaw/index.js
+++ client/src/components/Saginaw/index.js
@@ -1,27 +1,41 @@
-
 import React from 'react';
 
 export default function Nav() {
-    return (
-        <section class="hero is-fullheight">
-                <div class="hero-body">
-                    <div class="container has-text-centered">
-                        <div class="columns is-8 is-variable ">
-                            <div class="column is-two-thirds has-text-left">
-                                <h1 class="title is-1">Saginaw</h1>
-                                <p class="is-size-4">Dog-Friendly Places and Activities</p>
-                                <p class="is-size-5">Restaurants: Yogurt Yeti, G's Pizzeria Bar and Grill, Beef 'O' Brady's</p>
-                                <p class="is-size-5">Veterinarians: At Mid-Michigan Veterinary Hospital, Veterinary Health Center</p>
-                                <p class="is-size-5">Boarding Facilities: Woodland Pet Resort</p>
-                                <p class="is-size-5">Stores: PetSmart,Pet Supplies Plus, Magoos Pet Outlet</p>
-                                <p class="is-size-5">Groomers: </p>
-                                <p class="is-size-5">Hotels: </p>
-                                <p class="is-size-5">Parks: </p>
-                                <p class="is-size-5">Trails: </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-    )
+	return (
+		<section class='hero is-fullheight'>
+			<div class='hero-body'>
+				<div class='container has-text-centered'>
+					<div class='columns is-8 is-variable '>
+						<div class='column is-two-thirds has-text-left'>
+							<h1 class='title is-1'>Saginaw</h1>
+							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
+							<p class='is-size-5'>
+								Restaurants: Yogurt Yeti, G's Pizzeria Bar and Grill, Beef 'O'
+								Brady's
+							</p>
+							<p class='is-size-5'>
+								Veterinarians: At Mid-Michigan Veterinary Hospital, Veterinary
+								Health Center
+							</p>
+							<p class='is-size-5'>Boarding Facilities: Woodland Pet Resort</p>
+							<p class='is-size-5'>
+								Stores: PetSmart,Pet Supplies Plus, Magoos Pet Outlet
+							</p>
+							<p class='is-size-5'>
+								Groomers: Hairy Paws Day Center, Pretty Pooch, Shear Canine,
+								Woodland Pet Resort
+							</p>
+							<p class='is-size-5'>
+								Hotels: Relax Inn, Residence Inn by Marriott Saginaw, Comfort
+								Suites Saginaw
+							</p>
+							<p class='is-size-5'>
+								Parks: Steven A. Kirshenbaum Memorial Bark Park,
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
