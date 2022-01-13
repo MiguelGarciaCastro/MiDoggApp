94b758520e7be7a495f9910ed86104ac88ff574e
TS93 <tschram93@yahoo.com>
2022-01-13 16:17:55 -0500

    Marquette

--- client/src/components/Marquette/index.js
+++ client/src/components/Marquette/index.js
@@ -1,1 +1,32 @@
- 
+import React from 'react';
+
+export default function Nav() {
+	return (
+		<section class='hero is-fullheight'>
+			<div class='hero-body'>
+				<div class='container has-text-centered'>
+					<div class='columns is-8 is-variable '>
+						<div class='column is-two-thirds has-text-left'>
+							<h1 class='title is-1'>Marquette</h1>
+							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
+							<p class='is-size-5'>Restaurants: </p>
+							<p class='is-size-5'>Veterinarians: </p>
+							<p class='is-size-5'>Beaches: </p>
+							<p class='is-size-5'>Campgrounds: </p>
+							<p class='is-size-5'>
+								Boarding Facilities:
+							</p>
+							<p class='is-size-5'>Stores: </p>
+							<p class='is-size-5'>
+								Groomers: 
+							</p>
+							<p class='is-size-5'>Hotels: </p>
+							<p class='is-size-5'>Parks: </p>
+							<p class='is-size-5'>Trails: </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
