7ffd37ce7ecdda92c8a7023460d9c972763c7445
TS93 <tschram93@yahoo.com>
2022-01-13 16:23:08 -0500

    restaurants and vets added

--- client/src/components/Marquette/index.js
+++ client/src/components/Marquette/index.js
@@ -9,8 +9,8 @@ export default function Nav() {
 						<div class='column is-two-thirds has-text-left'>
 							<h1 class='title is-1'>Marquette</h1>
 							<p class='is-size-4'>Dog-Friendly Places and Activities</p>
-							<p class='is-size-5'>Restaurants: </p>
-							<p class='is-size-5'>Veterinarians: </p>
+							<p class='is-size-5'>Restaurants: The Delft Bistro, Dia De Los Tacos, Donckers, Kildare Irish American, Portside Inn, Lake Superior Smokehouse Brewpub </p>
+							<p class='is-size-5'>Veterinarians: Animal Medical Center, Marquette Veterinary Clinic, Bayshore Veterinary Hospital</p>
 							<p class='is-size-5'>Beaches: </p>
 							<p class='is-size-5'>Campgrounds: </p>
 							<p class='is-size-5'>
