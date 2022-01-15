9100add7ba060e295ad99bed1ffb90b602d58c7a
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-01-15 14:24:49 -0500

    Update home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,10 +1,14 @@
 import Home from "../components/Home";
+import Cities from "../components/Cities";
 import Cart from "../components/Cart";
 
 export default function homepage() {
     return (
         <div className="container">
-           <Home /> 
+           <Home />
+        </div>
+        <div className="container">
+           <Cities /> 
         </div>
     )
 }
